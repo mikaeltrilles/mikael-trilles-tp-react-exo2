76a8d3f83f1c05482df8a3c0359473ac79508f20
Mikael Trilles <98765121+mikaeltrilles@users.noreply.github.com>
2022-05-22 16:36:38 +0200

    reglage affichage

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     universities: [],
   };
 
-  
+
 
   //* Je récupère le pays choisi dans le select
   handleChangeSelect = (selectValue) => {
@@ -45,20 +45,20 @@ class App extends Component {
     let togg1 = document.getElementById("togg1");
     let d1 = document.getElementById("d1");
     togg1.addEventListener("click", () => {
-      if(getComputedStyle(d1).display != "none"){
-       d1.style.display = "none";
+      if (getComputedStyle(d1).display != "none") {
+        d1.style.display = "none";
       } else {
         d1.style.display = "block";
       }
     })
-  
+
 
     // console.log((click.target.value))
     // console.log(this.handleOnClickButton);
     return d1;
   };
 
-  
+
 
 
   //* Je crée un select pour choisir le pays entre : France, Italy, Germany
@@ -87,15 +87,15 @@ class App extends Component {
           {this.state.universities.length > 0 ? (
             <h1 className="text-success">{this.state.universities.length} Université(s) de trouvée(s)</h1>
           ) : (
-              <h1 className="text-danger">{this.state.universities.length} Université(s) de trouvée(s)</h1>
-            )}
+            <h1 className="text-danger">{this.state.universities.length} Université(s) de trouvée(s)</h1>
+          )}
           {(this.state.universities.length < 50 &&
             this.state.universities.length > 0) && (
-              <button 
-              id="togg1"
-              className="btn btn-secondary"
-              value="voir" onClick={this.handleOnClickButton}
-              display >
+              <button
+                id="togg1"
+                className="btn btn-secondary"
+                value="voir" onClick={this.handleOnClickButton}
+                display >
                 Voir les résultats
               </button>
             )}
@@ -103,27 +103,33 @@ class App extends Component {
 
           {/*Si le resultat sur le fecth est compris entre 1 et 50 et que il y a un click sur le bouton alors on affiche lle tableau des resultats */}
 
-          {(this.handleOnClickButton.d1) && (
-            <table className="table table-striped" id="d1" display='{this.handleOnClickButton ? "block" : "none"}'>
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Country</th>
-                  <th scope="col">Web</th>
+          {(true) &&   (this.state.universities.length < 50 &&
+            this.state.universities.length > 0) && (
+           
+            <div id="d1" className="table-responsive" style={
+              { display: "none" }
+            } >
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">Web</th>
                   </tr>
-              </thead>
-              <tbody>
-                {this.state.universities.map((university, index) => (
-                  <tr key={index}>
-                    <th scope="row">{index + 1}</th>
-                    <td>{university.name}</td>
-                    <td>{university.country}</td>
-                    <td><a href={university.web_pages[0]}>{university.web_pages[0]}</a></td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {this.state.universities.map((university, index) => (
+                    <tr key={index}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{university.name}</td>
+                      <td>{university.country}</td>
+                      <td><a href={university.web_pages[0]}>{university.web_pages[0]}</a></td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           )}
         </div>
       </>
